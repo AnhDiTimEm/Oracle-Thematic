f92c49d6e91ea053acc19546c60f1817e55e636d
Ân Pianist <ndsg1964@gmail.com>
2020-06-02 14:33:08 +0700

    Create Project

---
+++ OracleThematic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OracleThematic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
