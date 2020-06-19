c70fa5b83e9f32fb1a76c975b95f6c97722423ca
Ân Pianist <ndsg1964@gmail.com>
2020-06-19 11:09:00 +0700

    new template

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
     require_once("Config/Config.php");
     
     if(isset($_GET['chatpage'])){
-        require_once SITE_ROOT."/Views/chatpage.php";
+        require_once SITE_ROOT."/Views/chat.php";
+    }
+    if(isset($_GET['signup'])){
+        require_once SITE_ROOT."/Views/sign-up.php";
     }
     else{
 
-    require_once SITE_ROOT."/Views/login.php";
+    require_once SITE_ROOT."/Views/sign-in.php";
     }
 ?>
