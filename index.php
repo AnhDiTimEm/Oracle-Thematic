dd205fd543ed17907e178d29eb20845564432cf5
TranBaKhoa1999 <ohwhynotmee1999@gmail.com>
2020-06-19 11:04:07 +0700

    checking user

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
     if(isset($_GET['chatpage'])){
         require_once SITE_ROOT."/Controllers/ChatPageController.php";
     }
+    else if(isset($_GET['signup'])){
+        require_once SITE_ROOT."/Daos/UserDao";
+    }
+    else if(isset($_GET['signin'])){
+
+    }
     else{
 
     require_once SITE_ROOT."/Controllers/LoginController.php";
