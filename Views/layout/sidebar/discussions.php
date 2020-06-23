2bcb736759f0464451d24ab0d409cd102d2ceae8
TranBaKhoa1999 <ohwhynotmee1999@gmail.com>
2020-06-23 13:53:51 +0700

    dang lam ajax statusChat

--- Views/layout/sidebar/discussions.php
+++ Views/layout/sidebar/discussions.php
@@ -13,77 +13,69 @@
 	</div>						
 	<div class="discussions">
 		<h1>Discussions</h1>
-		<div class="list-group" id="chats" role="tablist">
+		<div id="showChatRoom"></div>
 		<?php
-			foreach ($allRoom as $key) {
-				$typeRoom = $roomDao->GetTypeOfRoom($key);
-				$memberList = $roomDao->GetAllMemberOfRoom($key);
-				if($typeRoom =="friend"){
-					foreach($memberList as $m){
-						if($m!=$_SESSION['user']){
-							$friend_Phone=$m;
+		echo 
+			'<div class="list-group" id="chats" role="tablist">';
+				foreach ($allRoom as $key) {
+					$typeRoom = $roomDao->GetTypeOfRoom($key);
+					$memberList = $roomDao->GetAllMemberOfRoom($key);
+					if($typeRoom =="friend"){
+						foreach($memberList as $m){
+							if($m!=$_SESSION['user']){
+								$friend_Phone=$m;
+							}
 						}
-					}
-					$friend = $dao->GetUserByPhone($friend_Phone);
-					echo'
-					<a href="';
-					if($friend->getStatus()=="offline")
-					{
-						echo'#list-empty-'.$key.'';
-						echo'" class="filterDiscussions all unread single" id="list-empty-list" data-toggle="list" role="tab">';
-					}
-					else{
-						echo'#list-chat-'.$key.'';
-						echo'" class="filterDiscussions all unread single" id="list-chat-list" data-toggle="list" role="tab">';
-					};
-					echo'
-						<img class="avatar-md" src="'.$friend->getAvatar().'" data-toggle="tooltip" data-placement="top" title="Michael" alt="avatar">
-						<div class="status">
-							<i class="material-icons ';
-					if($friend->getStatus()=="offline")
-					{
-						echo'offline';
-					}else{
-						echo'online';
-					};
-					echo'">fiber_manual_record</i>
+						$friend = $dao->GetUserByPhone($friend_Phone);
+						echo'
+						<a href="';
+						if($friend->getStatus()=="offline")
+						{
+							echo'#list-empty-'.$key.'';
+							echo'" class="filterDiscussions all unread single" id="list-empty-list" data-toggle="list" role="tab">';
+						}
+						else{
+							echo'#list-chat-'.$key.'';
+							echo'" class="filterDiscussions all unread single" id="list-chat-list" data-toggle="list" role="tab">';
+						};
+						echo'
+							<img class="avatar-md" src="'.$friend->getAvatar().'" data-toggle="tooltip" data-placement="top" title="Michael" alt="avatar">
+							<div class="status">
+								<i class="material-icons ';
+						if($friend->getStatus()=="offline")
+						{
+							echo'offline';
+						}else{
+							echo'online';
+						};
+						echo'">fiber_manual_record</i>
+							</div>';
+						echo'
+						<div class="data">
+							<h5>'.$friend->getName().'</h5>
+							<span>Sun</span>
+							<p>How can i improve my chances of getting a deposit?</p>
 						</div>';
-					echo'
-					<div class="data">
-						<h5>'.$friend->getName().'</h5>
-						<span>Sun</span>
-						<p>How can i improve my chances of getting a deposit?</p>
-					</div>';
-				}
-				else if($typeRoom == "group"){
-					echo'
-					<a href="#list-chat-'.$key.'" class="filterDiscussions all unread single" id="list-chat-list" data-toggle="list" role="tab">
-						<img class="avatar-md" src="./Resources/images/group.jpg" data-toggle="tooltip" data-placement="top" title="Michael" alt="avatar">
-						<div class="status">
-							<i class="material-icons online">fiber_manual_record</i>
+					}
+					else if($typeRoom == "group"){
+						echo'
+						<a href="#list-chat-'.$key.'" class="filterDiscussions all unread single" id="list-chat-list" data-toggle="list" role="tab">
+							<img class="avatar-md" src="./Resources/images/group.jpg" data-toggle="tooltip" data-placement="top" title="Michael" alt="avatar">
+							<div class="status">
+								<i class="material-icons online">fiber_manual_record</i>
+							</div>';
+						echo'
+						<div class="data">
+							<h5>'.'Group Code: '.$key.'</h5>
+							<span>Sun</span>
+							<p>How can i improve my chances of getting a deposit?</p>
 						</div>';
+					}
 					echo'
-					<div class="data">
-						<h5>'.'Group Code: '.$key.'</h5>
-						<span>Sun</span>
-						<p>How can i improve my chances of getting a deposit?</p>
-					</div>';
-				}
-				echo'
-			</a>
+					</a>
 				';
 			};
+		echo'</div>'
 		?>
-			<!-- <a href="#list-chat" class="filterDiscussions all unread single active" id="list-chat-list" data-toggle="list" role="tab">
-				<img class="avatar-md" src="./asset/img/avatars/avatar-female-1.jpg" data-toggle="tooltip" data-placement="top" title="Janette" alt="avatar">
-				<div class="status">
-					<i class="material-icons online">fiber_manual_record</i>
-				</div>
-				<div class="data">
-					<h5>Janette Dalton</h5>
-					<span>Mon</span>
-				</div>
-			</a>									 -->
-		</div>
 	</div>
 </div>
