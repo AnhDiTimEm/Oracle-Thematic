388a780348937d1bad015dabe4198773206c2975
TranBaKhoa1999 <ohwhynotmee1999@gmail.com>
2020-06-25 18:25:52 +0700

    xong xử lý Time chat

--- Views/statusChat_ajax.php
+++ Views/statusChat_ajax.php
@@ -17,6 +17,14 @@ if ($_SESSION['user'] != null)
             $typeRoom = $roomDao->GetTypeOfRoom($key);
             $memberList = $roomDao->GetAllMemberOfRoom($key);
             $friend_Phone="";
+            $allMess = $roomDao->GetAllMessByRoom($key);
+            $lastContent="";
+            $lastMemChat="";
+            if($allMess!=null){
+               $lastContent= end($allMess)->getContent();
+               $lastmem = $dao->getUSerByPhone(end($allMess)->getPhone_user());
+               $lastMemChat=$lastmem->getName();
+            }
             if($typeRoom =="friend"){
                 foreach($memberList as $m){
                     if($m!=$_SESSION['user']){
@@ -54,8 +62,81 @@ if ($_SESSION['user'] != null)
                 echo'
                 <div class="data">
                     <h5>'.$friend->getName().'</h5>
-                    <span>Sun</span>
-                    <p>How can i improve my chances of getting a deposit?</p>
+                    ';
+                    if($allMess!=null){
+                        date_default_timezone_set('Asia/Ho_Chi_Minh');
+                        // get thời gian chat ( date - now)
+                        $date1 = strtotime(end($allMess)->getTime()); // time chat
+                        $date2= time(); // now
+                        $TimeShow="";
+                        $diff = abs($date1 - $date2);  
+
+                        $years = floor($diff / (365*60*60*24));  
+
+                        $months = floor(($diff - $years * 365*60*60*24) 
+                                                    / (30*60*60*24));  
+
+                        $days = floor(($diff - $years * 365*60*60*24 -  
+                                    $months*30*60*60*24)/ (60*60*24)); 
+                        
+                        $hours = floor(($diff - $years * 365*60*60*24  
+                            - $months*30*60*60*24 - $days*60*60*24) 
+                                                        / (60*60));  
+                    
+                        $minutes = floor(($diff - $years * 365*60*60*24  
+                                - $months*30*60*60*24 - $days*60*60*24  
+                                                - $hours*60*60)/ 60);
+                        if($months==0){
+                            if($days==0){
+                                if($hours==0){
+                                    if($minutes==0){
+                                        $TimeShow="Just Now";
+                                    }
+                                    else{
+                                        if($minutes==1){
+                                            $TimeShow=$minutes.' minute ago';
+                                        }
+                                        else{
+                                            $TimeShow=$minutes.' minutes ago';
+                                        }
+                                    }
+                                }
+                                else{
+                                    if($hours==1){
+                                        $TimeShow=$hours.' hour ago';
+                                    }
+                                    else{
+                                        $TimeShow=$hours.' hours ago';
+                                    }
+                                }
+                            }
+                            else{
+                                if($days==1){
+                                    $TimeShow=$days.' day ago';
+                                }
+                                else{
+                                    $TimeShow=$days.' days ago';
+                                }
+                            }
+                        }
+                        else{
+                            if($months==1){
+                                $TimeShow=$months.' month ago';
+                            }
+                            else{
+                                $TimeShow=$months.' months ago';
+                            }
+                        }
+                        
+                        echo'
+                        <span>'.$TimeShow.'</span>
+                        <p>'.$lastMemChat.' : '.$lastContent.'</p>';
+                    }
+                    else{
+                        echo'
+                        <p>Lets Talk Together</p>';
+                    }
+                    echo'
                 </div>';
             }
             else if($typeRoom == "group"){
@@ -68,9 +149,81 @@ if ($_SESSION['user'] != null)
                     </div>';
                 echo'
                 <div class="data">
-                    <h5>'.'Group : '.$name.'</h5>
-                    <span>Sun</span>
-                    <p>How can i improve my chances of getting a deposit?</p>
+                    <h5>'.'Group : '.$name.'</h5>';
+                    if($allMess!=null){
+                        date_default_timezone_set('Asia/Ho_Chi_Minh');
+                        // get thời gian chat ( date - now)
+                        $date1 = strtotime(end($allMess)->getTime()); // time chat
+                        $date2= time(); // now
+                        $TimeShow="";
+                        $diff = abs($date1 - $date2);  
+
+                        $years = floor($diff / (365*60*60*24));  
+
+                        $months = floor(($diff - $years * 365*60*60*24) 
+                                                    / (30*60*60*24));  
+
+                        $days = floor(($diff - $years * 365*60*60*24 -  
+                                    $months*30*60*60*24)/ (60*60*24)); 
+                        
+                        $hours = floor(($diff - $years * 365*60*60*24  
+                            - $months*30*60*60*24 - $days*60*60*24) 
+                                                        / (60*60));  
+                    
+                        $minutes = floor(($diff - $years * 365*60*60*24  
+                                - $months*30*60*60*24 - $days*60*60*24  
+                                                - $hours*60*60)/ 60);
+                        if($months==0){
+                            if($days==0){
+                                if($hours==0){
+                                    if($minutes==0){
+                                        $TimeShow="Just Now";
+                                    }
+                                    else{
+                                        if($minutes==1){
+                                            $TimeShow=$minutes.' minute ago';
+                                        }
+                                        else{
+                                            $TimeShow=$minutes.' minutes ago';
+                                        }
+                                    }
+                                }
+                                else{
+                                    if($hours==1){
+                                        $TimeShow=$hours.' hour ago';
+                                    }
+                                    else{
+                                        $TimeShow=$hours.' hours ago';
+                                    }
+                                }
+                            }
+                            else{
+                                if($days==1){
+                                    $TimeShow=$days.' day ago';
+                                }
+                                else{
+                                    $TimeShow=$days.' days ago';
+                                }
+                            }
+                        }
+                        else{
+                            if($months==1){
+                                $TimeShow=$months.' month ago';
+                            }
+                            else{
+                                $TimeShow=$months.' months ago';
+                            }
+                        }
+                        
+                        echo'
+                        <span>'.$TimeShow.'</span>
+                        <p>'.$lastMemChat.' : '.$lastContent.'</p>';
+                    }
+                    else{
+                        echo'
+                        <p>Lets Talk Together</p>';
+                    }
+                    echo'
                 </div>';
             }
             echo'
